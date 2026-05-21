58515de0dc40434ca175817596f07287a7ae4f83
victo <victo@VICS>
2026-05-20 23:00:52 -0600

    Add project files.

---
+++ Repaso3P2/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace Repaso3P2.Models
+{
+    public class Animal
+    {
+        public string NombrePropio { get; set; } = string.Empty;
+
+        public string Especie { get; set; } = string.Empty;
+
+        public string TipoAlimentacion { get; set; } = string.Empty;
+
+        public int TiempoMaximoVida { get; set; }
+    }
+}
---
+++ Repaso3P2/Models/Ave.cs
@@ -0,0 +1,9 @@
+﻿namespace Repaso3P2.Models
+{
+    public class Ave : Animal
+    {
+        public bool Vuela { get; set; }
+
+        public int PeriodoIncubacion { get; set; }
+    }
+}
